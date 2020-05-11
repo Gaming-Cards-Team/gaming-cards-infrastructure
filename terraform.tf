aaae81b33727ed038fea5355d3e4b57bac648399
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-05-11 11:03:02 -0500

    NO-STORY [@twjmgorduez] Create infrastructure project

---
+++ terraform.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "0.12.24"
+  backend "s3" {
+    encrypt = true
+    region  = "us-east-2"
+    bucket  = "gaming-cards-terraform-remotestate"
+    key     = "gaming-cards/remote.tfstate"
+
+    dynamodb_table = "gaming-cards-terraform-statelock"
+  }
+}
