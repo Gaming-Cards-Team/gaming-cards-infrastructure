4efd5946f9a68793355e713a8dc5ddffa65c1718
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-05-15 23:27:08 -0500

    NO-STORY [@twjmgorduez] Code refactoring

--- variables.tf
+++ variables.tf
@@ -4,7 +4,6 @@ locals {
         project_name = "gaming-cards"
         instance_type = "t2.micro"
         ssh_user = "ubuntu"
-        ssh_key_name = "gaming_cards_generated_key"
         algorithm = "RSA"
         rsa_bits  = 4096
         bash_scripts_path = "bashScripts/"
@@ -13,11 +12,13 @@ locals {
         user_data_path = "userDataPro.sh"
         setup_script_path = "startupInstancePro.sh"
         public_ip = "3.16.209.207"
+        ssh_key_name = "gaming_cards_generated_key_pro"
     }
     qa    = {
         user_data_path = "userDataQa.sh"
         setup_script_path = "startupInstanceQa.sh"
         public_ip = "3.23.123.190"
+        ssh_key_name = "gaming_cards_generated_key_qa"
     }
   }
   setting = merge(local.settings["default"], local.settings[terraform.workspace])
